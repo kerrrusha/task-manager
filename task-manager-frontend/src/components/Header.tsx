f206dfac91e2363f541ed783f465f8d7085c15e5
Kirill Koval <brcyrill2@gmail.com>
2024-02-04 19:41:22 +0200

    header fixed

--- task-manager-frontend/src/components/Header.tsx
+++ task-manager-frontend/src/components/Header.tsx
@@ -30,31 +30,30 @@ export default function Header() {
                                     )}
                                 </Disclosure.Button>
                             </div>
-                            <div className="flex items-center justify-center sm:items-stretch sm:justify-start">
+                            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                                 <div className="flex flex-shrink-0 items-center">
                                     <img
                                         className="h-8 w-auto"
                                         src="https://cdn-icons-png.flaticon.com/512/5065/5065589.png"
                                         alt="Task Manager"
                                     />
-                                    <p className="font-bold font-sans sm:ml-6 text-white">Task Manager</p>
                                 </div>
-                            </div>
-                            <div className="flex flex-1 items-center justify-center sm:ml-6">
-                                <div className="flex space-x-4">
-                                    {navigation.map((item) => (
-                                        <a
-                                            key={item.name}
-                                            href={item.href}
-                                            className={classNames(
-                                                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                'rounded-md px-3 py-2 text-sm font-medium'
-                                            )}
-                                            aria-current={item.current ? 'page' : undefined}
-                                        >
-                                            {item.name}
-                                        </a>
-                                    ))}
+                                <div className="hidden sm:ml-6 sm:block">
+                                    <div className="flex space-x-4">
+                                        {navigation.map((item) => (
+                                            <a
+                                                key={item.name}
+                                                href={item.href}
+                                                className={classNames(
+                                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                    'rounded-md px-3 py-2 text-sm font-medium'
+                                                )}
+                                                aria-current={item.current ? 'page' : undefined}
+                                            >
+                                                {item.name}
+                                            </a>
+                                        ))}
+                                    </div>
                                 </div>
                             </div>
                             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
