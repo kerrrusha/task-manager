1843eeb1cb2c1fa6825f696ef3c4e11335a2c4f7
Kirill Koval <brcyrill2@gmail.com>
2024-02-06 01:29:51 +0200

    added KanbanTask component

--- task-manager-frontend/src/components/KanbanBoard.tsx
+++ task-manager-frontend/src/components/KanbanBoard.tsx
@@ -1,5 +1,6 @@
 import Sidebar from "./Sidebar";
 import {useState} from "react";
+import KanbanTask from "./KanbanTask";
 
 export default function KanbanBoard() {
     const [activeIndex, setActiveIndex] = useState(0);
@@ -7,9 +8,24 @@ export default function KanbanBoard() {
     return (
         <div className="main-board">
             <Sidebar activeIndex={activeIndex} callback={setActiveIndex} />
-            <div>
-                <div className="flex space-x-4 items-center md:space-x-6">
-                    <button className="button">+ Add New Task</button>
+            <div className="w-100 p-3">
+                <div className="d-flex flex-row justify-between w-100">
+                    <p className="background-primary font-bold fs-4 m-0">Platform Launch</p>
+                    <button className="button background-action">+ Add New Task</button>
+                </div>
+                <hr />
+                <div>
+                    <div className="scrollbar-hide mx-5 pt-[10px] min-w-[280px] ">
+                        <p className=" font-semibold flex  items-center  gap-2 tracking-widest md:tracking-[.2em] text-[#828fa3]">
+                            <div className="rounded-full w-4 h-4 bg-orange-500 "></div>
+                            Todo (4)
+                        </p>
+                        <KanbanTask />
+                        <KanbanTask />
+                        <KanbanTask />
+                        <KanbanTask />
+                        <KanbanTask />
+                    </div>
                 </div>
             </div>
         </div>
---
+++ task-manager-frontend/src/components/KanbanTask.tsx
@@ -0,0 +1,28 @@
+export default function KanbanTask() {
+    return (
+        <div>
+            <div draggable="true"
+                 className="background-secondary w-[280px] first:my-5 rounded-lg shadow-[#364e7e1a] pt-6 pb-2 px-3
+                 shadow-lg hover:text-[#635fc7] dark:text-white dark:hover:text-[#635fc7] cursor-pointer "
+            >
+                <p className="font-bold tracking-wide ">
+                    Finish frontend part of Kanban board
+                </p>
+                <p className="text-s tracking-tighter mt-2 text-gray-500">
+                    Complete board React component, sign-in, sign-out, user profile pages
+                </p>
+                <div>
+                    <p className="font-bold text-xs tracking-tighter mt-2 text-gray-500">
+                        Assigned to: <span>Kirill</span>
+                    </p>
+                    <p className="font-bold text-xs tracking-tighter mt-2 text-gray-500">
+                        Priority: <span>High</span>
+                    </p>
+                    <p className="font-bold text-xs tracking-tighter mt-2 text-gray-500">
+                        Due date: <span>2024-02-06</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
