97b8fde9f744285f9668feda759623cc9db65a53
Kirill Koval <brcyrill2@gmail.com>
2024-02-16 01:42:13 +0200

    refactor, now saves admin if not exists

--- task-manager-backend/src/main/java/com/kerrrusha/taskmanagerbackend/service/BootstrapService.java
+++ task-manager-backend/src/main/java/com/kerrrusha/taskmanagerbackend/service/BootstrapService.java
@@ -1,23 +1,42 @@
 package com.kerrrusha.taskmanagerbackend.service;
 
+import com.kerrrusha.taskmanagerbackend.domain.Role;
 import com.kerrrusha.taskmanagerbackend.domain.User;
 import com.kerrrusha.taskmanagerbackend.repository.UserRepository;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
+import java.util.Collections;
+
+@Slf4j
+@Component
 @RequiredArgsConstructor
-public class BootstrapService implements CommandLineRunner {
+public class BootstrapService implements ApplicationListener<ContextRefreshedEvent> {
 
     private final UserRepository userRepository;
 
     @Override
-    public void run(String... args) throws Exception {
-        User exampleUser = new User();
-        exampleUser.setEmail("example@mail.com");
-        exampleUser.setFirstName("First");
-        exampleUser.setLastName("Last");
-        exampleUser.setPassword("qwerty");
+    public void onApplicationEvent(@NonNull ContextRefreshedEvent event) {
+        saveAdminIfNotExists();
+    }
 
-        userRepository.save(exampleUser);
+    private void saveAdminIfNotExists() {
+        User admin = new User();
+        admin.setEmail("admin@admin.com");
+        admin.setFirstName("First");
+        admin.setLastName("Last");
+        admin.setPassword("qwerty");
+        admin.setRoles(Collections.singleton(Role.builder().name(Role.RoleName.ADMIN).build()));
+
+        if (userRepository.findByEmail(admin.getEmail()).isPresent()) {
+            return;
+        }
+
+        log.info("Saving user: {}", admin);
+        userRepository.save(admin);
     }
 }
