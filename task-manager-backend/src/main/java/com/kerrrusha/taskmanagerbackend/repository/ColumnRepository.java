4f2b8bc70b972baa5e653c6b2ca39c3ce9d7b094
Kirill Koval <brcyrill2@gmail.com>
2024-02-17 19:16:04 +0200

    removed redundant repos

--- task-manager-backend/src/main/java/com/kerrrusha/taskmanagerbackend/repository/ColumnRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kerrrusha.taskmanagerbackend.repository;
-
-import com.kerrrusha.taskmanagerbackend.domain.Column;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ColumnRepository extends MongoRepository<Column, String> {
-}
