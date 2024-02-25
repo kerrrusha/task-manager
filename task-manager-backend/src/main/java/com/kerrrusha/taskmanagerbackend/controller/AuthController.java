7bd3fb1167e26f97c24fb7ea982d8a3787079763
Kirill Koval <brcyrill2@gmail.com>
2024-02-25 03:00:41 +0200

    method moved

--- task-manager-backend/src/main/java/com/kerrrusha/taskmanagerbackend/controller/AuthController.java
+++ task-manager-backend/src/main/java/com/kerrrusha/taskmanagerbackend/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.kerrrusha.taskmanagerbackend.controller;
 
+import com.google.common.net.HttpHeaders;
 import com.kerrrusha.taskmanagerbackend.dto.auth.JwtAuthenticationResponse;
 import com.kerrrusha.taskmanagerbackend.dto.user.request.UserLoginRequestDto;
 import com.kerrrusha.taskmanagerbackend.dto.user.request.UserRegistrationRequestDto;
@@ -8,12 +9,21 @@ import com.kerrrusha.taskmanagerbackend.security.AuthenticationService;
 import com.kerrrusha.taskmanagerbackend.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.kerrrusha.taskmanagerbackend.controller.OAuthController.AUTH_TOKEN;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,6 +33,7 @@ public class AuthController {
 
     private final UserService userService;
     private final AuthenticationService authenticationService;
+    private final SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
 
     @PostMapping("/register")
     @Operation(summary = "Register a new user")
@@ -34,5 +45,19 @@ public class AuthController {
     @Operation(summary = "Authenticate a user")
     public JwtAuthenticationResponse login(@Valid @RequestBody UserLoginRequestDto request) {
         return authenticationService.authenticate(request);
+    }
+
+    @PostMapping("/logout")
+    @ResponseStatus(HttpStatus.OK)
+    public void performLogout(Authentication authentication, HttpServletRequest request, HttpServletResponse response) {
+        final ResponseCookie cookie = ResponseCookie.from(AUTH_TOKEN, "")
+                .httpOnly(true)
+                .maxAge(0)
+                .path("/")
+                .secure(false)
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
+        logoutHandler.logout(request, response, authentication);
     }
 }
--- task-manager-backend/src/main/java/com/kerrrusha/taskmanagerbackend/controller/OAuthController.java
+++ task-manager-backend/src/main/java/com/kerrrusha/taskmanagerbackend/controller/OAuthController.java
@@ -2,20 +2,15 @@ package com.kerrrusha.taskmanagerbackend.controller;
 
 import com.google.common.net.HttpHeaders;
 import com.kerrrusha.taskmanagerbackend.dto.oauth.GoogleOAuthLoginRequestDto;
-import com.kerrrusha.taskmanagerbackend.dto.user.response.UserResponseDto;
 import com.kerrrusha.taskmanagerbackend.security.GoogleOAuthService;
-import com.kerrrusha.taskmanagerbackend.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,13 +20,6 @@ public class OAuthController {
     public static final String AUTH_TOKEN = "AUTH-TOKEN";
 
     private final GoogleOAuthService googleOAuthService;
-    private final UserService userService;
-
-    @GetMapping("/user/info")
-    public UserResponseDto getUserInfo(Principal principal) {
-        String userId = principal.getName();
-        return userService.findByEmail(userId);
-    }
 
     @PostMapping("/google/login")
     public ResponseEntity<Void> loginGoogle(@RequestBody GoogleOAuthLoginRequestDto requestBody, HttpServletResponse response) {
