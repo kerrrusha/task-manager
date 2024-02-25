71c0fe6c883b867b85ec703bd9ff61d8e7c1538a
Kirill Koval <brcyrill2@gmail.com>
2024-02-25 20:42:03 +0200

    added error boundary

--- task-manager-frontend/src/index.tsx
+++ task-manager-frontend/src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ErrorInfo} from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './tailwind.css';
@@ -7,14 +7,23 @@ import './index.css';
 import './App.css';
 import store from "./redux/store";
 import {Provider} from "react-redux";
+import {ErrorBoundary} from "react-error-boundary";
+import ErrorPage from "./pages/ErrorPage";
+
+function handleError(error: Error, info: ErrorInfo) {
+    console.error("Caught an error:", error, info);
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-      <Provider store={store}>
-          <App />
-      </Provider>
+      <ErrorBoundary FallbackComponent={ErrorPage} onError={handleError}>
+          <Provider store={store}>
+              <App />
+          </Provider>
+      </ErrorBoundary>
   </React.StrictMode>
 );
