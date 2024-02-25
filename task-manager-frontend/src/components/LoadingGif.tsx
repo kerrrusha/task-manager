157eeff846cfac9b41362c1e5e84be83f3f00872
Kirill Koval <brcyrill2@gmail.com>
2024-02-26 00:52:26 +0200

    fixed bug with big gif

--- task-manager-frontend/src/components/LoadingGif.tsx
+++ task-manager-frontend/src/components/LoadingGif.tsx
@@ -2,8 +2,9 @@ interface LoadingGifProps {
     width?: number;
 }
 
-export default function LoadingGif({width = 50} : LoadingGifProps) {
+export default function LoadingGif({width=50} : LoadingGifProps) {
+// noinspection JSSuspiciousNameCombination
     return <img alt="loading.gif"
          src="https://github.com/kerrrusha/wot-stat-tracker/blob/master/wot-stat-tracker-web/src/main/resources/static/gif/loading.gif?raw=true"
-         style={{width: width}} />;
+         style={{width: width, height: width}} />;
 }
